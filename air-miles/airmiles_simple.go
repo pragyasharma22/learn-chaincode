f6ad6ed487f22ec76cb97fa1c83c624c491c24be
pragyasharma22 <pragya.c.sharma@accenture.com>
2016-08-23 16:19:42 +0530

    1

--- air-miles/airmiles_simple.go
+++ air-miles/airmiles_simple.go
@@ -240,7 +240,7 @@ func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte,
 	}
 
 	key = args[0]
-	valAsbytes, err := stub.GetState(A)
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
